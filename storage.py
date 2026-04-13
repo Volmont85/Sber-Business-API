03e4595eee14fad3c8eb3fcd2841c76cd461ccda
Volmont85 <volmont85@gmail.com>
2026-04-13 15:45:54 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,6 +1,5 @@
 import redis
-from datetime import datetime
-
+from datetime import date
 
 class Storage:
 
@@ -8,8 +7,36 @@ class Storage:
 
         self.redis = redis.from_url(redis_url)
 
-    def lock_today(self):
+    def get_external_id(self):
 
-        key = "overnight:" + datetime.now().strftime("%Y-%m-%d")
+        return f"overnight-{date.today()}"
 
-        return self.redis.setnx(key, "1")
+    def acquire_lock(self):
+
+        key = f"overnight-lock:{date.today()}"
+        return self.redis.set(key, "1", nx=True, ex=7200)
+
+    def release_lock(self):
+
+        key = f"overnight-lock:{date.today()}"
+        self.redis.delete(key)
+
+    def mark_application_created(self):
+
+        key = f"overnight-created:{date.today()}"
+        self.redis.set(key, "1", ex=86400)
+
+    def application_created(self):
+
+        key = f"overnight-created:{date.today()}"
+        return self.redis.exists(key)
+
+    def mark_completed(self):
+
+        key = f"overnight-complete:{date.today()}"
+        self.redis.set(key, "1", ex=86400)
+
+    def already_completed(self):
+
+        key = f"overnight-complete:{date.today()}"
+        return self.redis.exists(key)
