25eb4db14c179edf282eae3b308ad8d5786abf6c
Volmont85 <volmont85@gmail.com>
2026-04-14 18:28:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
     MOSCOW_TZ = "Europe/Moscow"
 
     RUN_HOUR = 18
-    RUN_MINUTE = 5
+    RUN_MINUTE = 30
 
     CUT_OFF_HOUR = 18
-    CUT_OFF_MINUTE = 30
+    CUT_OFF_MINUTE = 40
